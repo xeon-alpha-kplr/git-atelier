e82d27952c61ca41e5b7eab5220b7b73a23709ec
labriet martin <martinlabriet@gmail.com>
2023-06-26 12:08:52 +0000

    Ajout de la fonction de division

--- calculatrice.py
+++ calculatrice.py
@@ -7,3 +7,10 @@ def soustraction(a, b):
 
 def multiplication(a, b):
     return a * b
+
+
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Erreur : Division par zéro"
