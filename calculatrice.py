7a83c6c18216905cfe22d71b10dd28f58e7598aa
labriet martin <martinlabriet@gmail.com>
2023-06-26 12:09:44 +0000

    Ajout de la fonction de calculatrice

--- calculatrice.py
+++ calculatrice.py
@@ -14,3 +14,41 @@ def division(a, b):
         return a / b
     else:
         return "Erreur : Division par zéro"
+
+
+def calculatrice():
+    print("Bienvenue dans la calculatrice !")
+    print("Veuillez choisir une opération :")
+    print("1. Addition")
+    print("2. Soustraction")
+    print("3. Multiplication")
+    print("4. Division")
+    choix = input("Votre choix (1-4) : ")
+
+    if choix == "1":
+        a = float(input("Premier nombre : "))
+        b = float(input("Deuxième nombre : "))
+        resultat = addition(a, b)
+        print("Résultat : ", resultat)
+    elif choix == "2":
+        a = float(input("Premier nombre : "))
+        b = float(input("Deuxième nombre : "))
+        resultat = soustraction(a, b)
+        print("Résultat : ", resultat)
+    elif choix == "3":
+        a = float(input("Premier nombre : "))
+        b = float(input("Deuxième nombre : "))
+        resultat = multiplication(a, b)
+        print("Résultat : ", resultat)
+    elif choix == "4":
+        a = float(input("Numérateur : "))
+        b = float(input("Dénominateur : "))
+        if b != 0:
+            resultat = division(a, b)
+            print("Résultat : ", resultat)
+        else:
+            print("Erreur : Division par zéro")
+    else:
+        print("Choix invalide.")
+
+calculatrice()
